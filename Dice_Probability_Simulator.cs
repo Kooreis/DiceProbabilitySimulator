0676b40cb2e205693bbeea0be7fa5ae3136234fa
Kooreis <kooreis.official@gmail.com>
2023-09-15 10:21:08 -0500

    Initial setup of the DiceProbabilitySimulator namespace and Program class. A Random object and an integer array to hold dice roll counts are declared.

--- Dice_Probability_Simulator.cs
+++ Dice_Probability_Simulator.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 namespace DiceProbabilitySimulator
@@ -9,22 +8,6 @@ namespace DiceProbabilitySimulator
         {
             Random random = new Random();
             int[] diceRolls = new int[6];
-            Console.WriteLine("Enter the number of dice rolls:");
-            int numberOfRolls = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < numberOfRolls; i++)
-            {
-                int roll = random.Next(1, 7);
-                diceRolls[roll - 1]++;
-            }
-
-            Console.WriteLine("Dice Roll Probability Simulator Results:");
-            for (int i = 0; i < diceRolls.Length; i++)
-            {
-                double probability = (double)diceRolls[i] / numberOfRolls;
-                Console.WriteLine($"Number {i + 1}: {probability * 100}%");
-            }
         }
     }
 }
-```
