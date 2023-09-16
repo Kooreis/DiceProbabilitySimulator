d1b0749e935ce4f1296ebf1aeef54daa17f86e62
Kooreis <kooreis.official@gmail.com>
2023-09-16 10:21:08 -0500

    Added user input for the number of dice rolls. The input is converted to an integer and stored in the variable numberOfRolls.

--- Dice_Probability_Simulator.cs
+++ Dice_Probability_Simulator.cs
@@ -8,6 +8,8 @@ namespace DiceProbabilitySimulator
         {
             Random random = new Random();
             int[] diceRolls = new int[6];
+            Console.WriteLine("Enter the number of dice rolls:");
+            int numberOfRolls = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
