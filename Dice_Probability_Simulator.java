dd276bef94ea0c56cedd58752b33be0fbd326392
Kooreis <kooreis.official@gmail.com>
2023-09-16 10:21:08 -0500

    Defined the main class DiceProbabilitySimulator and the main method. Initialized the Scanner, Random objects and an array to keep track of dice roll counts.

--- Dice_Probability_Simulator.java
+++ Dice_Probability_Simulator.java
@@ -1,2 +1,5 @@
-import java.util.Random;
-import java.util.Scanner;
+public class DiceProbabilitySimulator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int[] diceCount = new int[13];
